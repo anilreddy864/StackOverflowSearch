b42809cab0702da3f1eea3ae3c5f8f36c18124a0
Anil Reddy <anilreddy864@gmail.com>
2020-01-13 16:48:53 +0530

    Signed-off-by: Anil Reddy <anilreddy864@gmail.com>

--- StackOverflowSearch/settings.py
+++ StackOverflowSearch/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'et6v#fq=!jezo-64!*_$n^z%c5k284ub1-xd%vn^)tgd8=9!2v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["35.225.21.54"]
 
 # Application definition
 
